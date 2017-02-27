23bbb1a6ecea5dcd918cd9d9b75cac6ca91f7a5a
rriegl <rieglraymund@gmail.com>
2017-02-27 16:21:44 -0600

    Inital commit
    Inital commit

---
+++ Witching Crilce/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour 
+{
+	public GameObject DialogBox;
+	public GameObject CharacterNameBox;
+	public Text CharacterDialog;
+	public Text CharacterName;
+
+	public bool DialogBoxActive;
+	public bool CharacterNameBoxActive;
+
+	public string[] DialogLines;
+	public int CourrentLine; // keeps track of courent line
+
+	// Use this for initialization
+	void Start () 
+	{
+		FindObjectsOfType<DialogManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (DialogBoxActive && Input.GetKeyDown(KeyCode.Space)) 
+		{
+			 	CourrentLine++; // increment lines 
+		}
+		if (CourrentLine >= DialogLines.Length) 
+		{
+			DialogBox.SetActive (false);
+			DialogBoxActive = false;
+
+			CourrentLine = 0;
+		}
+		CharacterDialog.text = DialogLines [CourrentLine];
+	}
+	public void ShowDialogBox(string Dialog)
+	{
+		DialogBoxActive = true;
+		DialogBox.SetActive(true);
+		CharacterDialog.text = Dialog;
+	}
+}
